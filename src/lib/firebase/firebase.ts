376eef62aa47a3b3e2b5304706c646f92462c74f
Dinica Madalin <dinicamadalin25@gmail.com>
2025-06-06 22:29:26 +0300

    Code Changes

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 console.log("Firebase config:", firebaseConfig);
-// Initialize Firebase
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
